ba9927f7b8f364284e84477388d7be5b5ef8fd44
Jose Guadalupe Cortes Cruz <joseegccr@gmail.com>
2022-08-02 22:54:48 -0500

    Creando test de Views

--- projectDjangoApp/polls/tests.py
+++ projectDjangoApp/polls/tests.py
@@ -1,7 +1,9 @@
 import datetime
+from urllib import response
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls.base import reverse
 
 from .models import Question
 
@@ -13,3 +15,11 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="¿Quien es el mejor Course Director de Platzi?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionIndexViewTest(TestCase):
+    def test_no_questions(self):
+        """if no question exist, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
